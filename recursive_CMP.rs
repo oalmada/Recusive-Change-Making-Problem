1f037b2d279c9134663d5a185e0df4ad56883c29
Omer <omer.y.almadani@gmail.com>
2017-11-26 15:27:06 -0500

    second commit

--- recursive_CMP.rs
+++ recursive_CMP.rs
@@ -7,20 +7,20 @@ fn main() {
     let n: u32 = args[1].parse::<u32>().unwrap();
     let l: u32 = args[2].parse::<u32>().unwrap();
 
-    let mut p: Vec<i32> = Vec::new();
+    let mut p: Vec<i64> = Vec::new();
 
     for i in 3..args.len() {
-        p.push(args[i].parse::<i32>().unwrap());
+        p.push(args[i].parse::<i64>().unwrap());
     }
 
     println!("{} {} {:?}", n, l, p);
 
-    let coin_count: u32 = recursive_change_make(n as i32, l as i32, &p);
+    let coin_count: u32 = recursive_change_make(n as i64, (l - 1) as i64, &p);
 
     println!("{}", coin_count);
 }
 
-fn recursive_change_make(mut n: i32, mut l: i32, p: &Vec<i32>) -> u32 {
+fn recursive_change_make(n: i64, l: i64, p: &Vec<i64>) -> u32 {
     if n == 0 {
         return 0;
     }
@@ -36,21 +36,19 @@ fn recursive_change_make(mut n: i32, mut l: i32, p: &Vec<i32>) -> u32 {
     let mut index: u32 = 0;
 
     for i in 0..p.len() {
-        if i as i32 == l {
+        if i as i64 == l {
             index = i as u32;
         }
     }
 
-    let mut left: u32 = recursive_change_make(n, l - 1, &p);
-    let mut right: u32 = recursive_change_make(n - &p[index as usize], l, &p);
+    let left: u32 = recursive_change_make(n, l - 1, &p);
+    let right: u32 = recursive_change_make(n - &p[index as usize], l, &p);
 
     let max: u32 = <u32>::max_value();
 
     if right != max {
-        right += 1;
+        return cmp::min(left, right + 1);
     }
 
-    //return cmp::min(recursive_change_make(n, l - 1, &p),
-    //                recursive_change_make(n - &p[index as usize], l, &p) + 1);
     return cmp::min(left, right);
 }
